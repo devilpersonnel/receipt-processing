92e2f10b8ea3ca0061de27b03076127087c323f4
Sushil Shrestha <sushil10018@gmail.com>
2014-07-23 18:26:43 +0545

    added user model with required attributes and validations

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+
+  has_secure_password(validations: false)
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true, uniqueness: true
+  validates :password, presence: true, length: { :in => 8..20 }, :on => :create
+
+  before_create :generate_api_token, :generate_api_secret
+
+  def self.rest_authenticate(path, timestamp, api_token, hash_string)
+    user = User.find_by(api_token: api_token).last
+    if user.present?
+      api_secret      = user.api_secret
+      request_details = path + timestamp
+      digested_hash   = Base64.strict_encode64(OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), api_secret, request_details))
+      if hash_string == digested_hash
+        user
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
+
+  private
+
+  def generate_api_token
+    begin
+      self.api_token = SecureRandom.hex
+    end while self.class.find_by(api_token: api_token).present?
+  end
+
+  def generate_api_secret
+    begin
+      self.api_secret = SecureRandom.base64(32)
+    end while self.class.find_by(api_secret: api_secret).present?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140723122020) do
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "api_token"
+    t.string   "api_secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
 
 end
