fcfc2a577d5ab81db303dc100a4fea38c81505e8
Sushil Shrestha <sushil10018@gmail.com>
2014-08-04 16:47:33 +0545

    tesseract path to be determined

--- lib/image_processor.rb
+++ lib/image_processor.rb
@@ -6,6 +6,9 @@ class ImageProcessor
 
   def extract_text
     e = Tesseract::Engine.new {|e|
+      if Rails.env == "production"
+        e.path = ENV['TESSDATA_PREFIX']
+      end
       e.language  = :eng
     }
     img =  Magick::Image.read(@receipt_image.path).first
