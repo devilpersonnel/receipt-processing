76814022f8a926830b34feadc0b0123622f486ce
devilpersonnel <devilpersonnel@gmail.com>
2016-08-23 11:22:05 +0545

    serving static files is set to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.public_file_server.enabled  = true
+  config.serve_static_files = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
