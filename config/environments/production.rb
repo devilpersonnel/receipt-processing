e1353c01010bca6fa6b45a23b1d4d12c876a8412
devilpersonnel <devilpersonnel@gmail.com>
2016-08-23 11:15:16 +0545

    config public file server enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
+  config.public_file_server.enabled  = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
